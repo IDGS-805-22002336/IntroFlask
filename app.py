34a25e5badaaea84b23ad1071874563ca912cbd6
RosarioTeran <lluviamares8@gmail.com>
2026-01-24 11:33:56 -0600

    Segundo commit trabajo en clase

---
+++ app.py
@@ -0,0 +1,68 @@
+
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def index():
+    titulo="flask IDGS805"
+    lista=["juan","pedro","mario"]
+    return render_template("index.html",titulo=titulo,lista=lista)
+
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html")
+
+
+@app.route("/hola")
+def hola():
+    return "<h1>--- Hola nuevo ---<h1>"
+
+@app.route("/user/<int:id>/<string:name>")
+def user(id,name):
+    return "ID: {} Nombre: {}".format(id,name)
+
+@app.route("/formulario")
+def formulario():  
+      return '''
+        <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+       
+        <label for="name">apaterno:</label>
+        <input type="text" id="name" name="name" required>
+    </form>
+    
+    '''
+
+@app.route("/operasBas")
+def opera1():
+    res=0
+    if request.method=="POST":
+        n1 = request.form.get("n1")
+        n2 = request.form.get("n2")
+        if n1 and n2:
+         res=int(n1) + int(n2)
+        
+    return render_template("operasBas.html",res=res)
+
+@app.route("/resultado", methods=['GET','POST'])
+def resultado():
+    n1 = int(request.form.get("n1", 0))
+    n2 = int(request.form.get("n2", 0))
+    res=n1+n2
+    return "la suma de {} + {} ={}".format(n1,n2,res)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
+'''
+hacer repositorio y hacer commit 
+'''
